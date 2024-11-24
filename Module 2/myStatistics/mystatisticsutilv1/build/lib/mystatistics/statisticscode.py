13077aef208b8422f30e7afa4091bf059eb45478
Jerica Olsen <114546897+JericaOlsen@users.noreply.github.com>
2024-11-23 18:29:22 -0700

    Create statisticscode.py

---
+++ Module 2/myStatistics/mystatisticsutilv1/build/lib/mystatistics/statisticscode.py
@@ -0,0 +1,13 @@
+import statistics
+
+def mean(data):
+    
+    return statistics.mean(data)
+
+def mode(data):
+
+    return statistics.mode(data)
+
+def median(data):
+
+    return statistics.median(data)
